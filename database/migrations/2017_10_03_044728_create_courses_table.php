2aa320ea5e198e3b4c89b22b261bd1cd80771c1c
Kenneth Sinder <kennethsinder@gmail.com>
2017-10-03 21:47:50 -0700

    Course model and migration

---
+++ database/migrations/2017_10_03_044728_create_courses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('subject');
+            $table->string('catalog_number');
+            $table->string('title');
+            $table->mediumText('description');
+            $table->decimal('units', 5, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
