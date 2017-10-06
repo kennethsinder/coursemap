b3cd2ec919febbab64613a856203866693209329
Kenneth Sinder <kennethsinder@gmail.com>
2017-10-05 20:13:15 -0700

    Courses import

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -6,13 +6,57 @@ use Illuminate\Http\Request;
 
 class CoursesController extends Controller
 {
-    public function getAllCourses()
+    private function getUwApiResponse($query)
     {
         $client = new \GuzzleHttp\Client;
-        $url = 'https://api.uwaterloo.ca/v2/courses.json?key=' . env('UW_API_KEY', '');
+        $url = "https://api.uwaterloo.ca/v2/$query.json?key=" . env('UW_API_KEY', '');
         $settings = ['verify' => false];
-        $response = $client->get($url, $settings);
+        try {
+            $response = $client->get($url, $settings);
+        }
+        catch (\Exception $e) {
+            return [];
+        }
 
-        return json_decode($response->getBody(), true);
+        if ($response->getStatusCode() != 200) {
+            // TODO: error handling
+            return [];
+        }
+
+        return json_decode($response->getBody(), true)['data'];
+    }
+
+    private function importCourses()
+    {
+        $courses = $this->getUwApiResponse('courses');
+        $subjects = [];
+
+        foreach ($courses as $course) {
+            if (!in_array($course['subject'], $subjects)) {
+                array_push($subjects, $course['subject']);
+            }
+        }
+
+        $courses = [];
+        foreach ($subjects as $subject) {
+            $coursesFromSubject = $this->getUwApiResponse("courses/$subject");
+            foreach ($coursesFromSubject as $course) {
+                $courseObj = \App\Course::FirstOrNew([
+                    'subject' => $course['subject'], 
+                    'catalog_number' => $course['catalog_number']
+                ], [
+                    'title' => $course['title'],
+                    'units' => $course['units'],
+                    'description' => $course['description'],
+                ]);
+
+                $courseObj->save();
+            }
+        }
+    }
+
+    public function getAllCourses()
+    {
+        return \App\Course::all();
     }
 }
