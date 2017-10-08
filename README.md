3a07ccd7288f228ebe9d45e24db070a3f490ffa5
ksinder <kennethsinder@gmail.com>
2017-10-08 00:08:26 -0400

    Update dev spinup steps and fix material

--- README.md
+++ README.md
@@ -7,6 +7,16 @@ To quickly spin up a development server:
 
 1. `npm install`
 
-2. `ng build`
+2. `composer install`
 
-3. `php artisan serve`
+3. `npm install -g @angular/cli`
+
+4. `ng build`
+
+5. `php artisan key:generate`
+
+6. Change DB password, etc. in `.env` file, and `create database if not exists 'coursemap'`
+
+7. `php artisan migrate`
+
+8. `php artisan serve`
--- angular/app/app.module.ts
+++ angular/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { MdButtonModule, MdCheckboxModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -17,12 +17,12 @@ import { CoursesService } from './courses.service';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MdButtonModule,
-    MdCheckboxModule
+    MatButtonModule,
+    MatCheckboxModule
   ],
   exports: [
-    MdButtonModule,
-    MdCheckboxModule
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [CoursesService],
   bootstrap: [AppComponent]
